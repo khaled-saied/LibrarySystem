bffe251ae853d43dbab9396dd23a242642b9503c
khaled saied <khaledsaied080@gmail.com>
2026-06-24 03:11:21 +0300

    2# Add Class Librarian

---
+++ LibrarySystem/Models/Librarian.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Models
+{
+    public class Librarian : LibraryUser
+    {
+        public string LibrarianId { get; set; } = string.Empty;
+        public decimal Salary { get; set; } 
+        public DateOnly HireDate { get; set; }
+
+
+
+        public Librarian(string name, string phone, decimal salary, DateOnly hireDate, string librarianId) : base(name, phone)
+        {
+            Salary = salary;
+            HireDate = hireDate;
+            LibrarianId = librarianId;
+        }
+
+        public override string ToDisplayString()
+        {
+            return $@"ID : {LibrarianId}
+                    Name : {Name} 
+                    Phone : {Phone}
+                    Salary : {Salary:c}
+                    Hired : {HireDate:dd/MM/yyy}";
+        }
+    }
+}
---
+++ LibrarySystem/Models/LibraryUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibrarySystem.Contracts;
+
+namespace LibrarySystem.Models
+{
+    public abstract class LibraryUser : IDisplayable
+    {
+        protected LibraryUser(string name, string phone)
+        {
+            Name = name;
+            Phone = phone;
+        }
+
+        public string Name { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+
+        public abstract string ToDisplayString();
+
+    }
+}
