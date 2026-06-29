747ccec74537087806cce3c419d73fa768340453
khaled saied <khaledsaied080@gmail.com>
2026-06-29 03:02:42 +0300

    2###Add Class LibraryService and Extenstions Method For Class

--- LibrarySystem/Extenstions/StringExtenstions.cs
+++ LibrarySystem/Extenstions/StringExtenstions.cs
@@ -12,5 +12,31 @@ namespace LibrarySystem.Extenstions
         {
             return input?.Trim().ToUpperInvariant() ?? string.Empty;
         }
+
+        public static bool ContainDigit(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input[i]))
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool IsValidEmail(this string input)
+        {
+            bool hasAt = false;
+            bool hasDot = false;
+            foreach (char c in input)
+            {
+                if (c == '.')
+                    hasDot = true;
+                else if (c == '@')
+                    hasAt = true;
+            }
+            return hasAt && hasDot;
+        }
     }
 }
